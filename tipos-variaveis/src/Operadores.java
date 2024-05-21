4fb2eea917b1218f9fbf8d7a0e439351ad5e238e
dlucioyauh <dlucio.douglas@gmail.com>
2024-05-20 23:25:28 -0300

    operadores java 4

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,13 +1,11 @@
 public class Operadores {
 
     public static void main(String[] args) {
-        int numero = 5;
+        boolean variavel = true;
 
-        numero = - numero;
-        System.out.println(numero);
+        variavel = !variavel;
 
-        numero =  numero * -1;
-        System.out.println(numero);
+        System.out.println(variavel);  
     }
-    
+
 }
